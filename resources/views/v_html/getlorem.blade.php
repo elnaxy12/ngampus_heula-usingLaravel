9b44c973d2f9ef847530cad015c72f8d61c81aa2
elnaxy12 <aryagilang533@gmail.com>
2025-09-30 17:45:41 +0700

    git init

--- resources/views/v_html/getlorem.blade.php
+++ resources/views/v_html/getlorem.blade.php
@@ -1,11 +1,36 @@
-<p style="font-weight: bold;">Mengenal huruf tebal</p>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi debitis voluptatum eius libero iure fugit facere in, reiciendis deserunt dolores?</p>
-<p style="text-decoration: underline; margin: 0;">Mengenal huruf garis bawah</p>
-<p style="margin: 0;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi debitis voluptatum eius libero iure fugit facere in, reiciendis deserunt dolores?</p>
-<p><em>Mengenal huruf miring</em> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi debitis voluptatum eius libero iure fugit facere in, reiciendis deserunt dolores?</p>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+    </head>
+    <body>
+        <p style="font-weight: bold">Mengenal huruf tebal</p>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi
+            debitis voluptatum eius libero iure fugit facere in, reiciendis
+            deserunt dolores?
+        </p>
+        <p style="text-decoration: underline; margin: 0">
+            Mengenal huruf garis bawah
+        </p>
+        <p style="margin: 0">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi
+            debitis voluptatum eius libero iure fugit facere in, reiciendis
+            deserunt dolores?
+        </p>
+        <p>
+            <em>Mengenal huruf miring</em>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi
+            debitis voluptatum eius libero iure fugit facere in, reiciendis
+            deserunt dolores?
+        </p>
 
-<style>
-    p {
-        width: 500px;
-    }
-</style>
+        <style>
+            p {
+                width: 500px;
+            }
+        </style>
+    </body>
+</html>
