cec374fdf9f53d900ae7cfba7a56b835e2351d9f
elnaxy12 <aryagilang533@gmail.com>
2025-09-30 17:33:36 +0700

    git init

---
+++ resources/views/v_html/getlorem.blade.php
@@ -0,0 +1,11 @@
+<p style="font-weight: bold;">Mengenal huruf tebal</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi debitis voluptatum eius libero iure fugit facere in, reiciendis deserunt dolores?</p>
+<p style="text-decoration: underline; margin: 0;">Mengenal huruf garis bawah</p>
+<p style="margin: 0;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi debitis voluptatum eius libero iure fugit facere in, reiciendis deserunt dolores?</p>
+<p><em>Mengenal huruf miring</em> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi debitis voluptatum eius libero iure fugit facere in, reiciendis deserunt dolores?</p>
+
+<style>
+    p {
+        width: 500px;
+    }
+</style>
